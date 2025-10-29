94cad2e12c65af4cacbf234b4ec1d72611e37718
Alice Maria <alicesantos@acad.ifma.edu.br>
2025-10-29 16:46:12 -0300

    Update tests.py

--- backend/eventos_app/tests.py
+++ backend/eventos_app/tests.py
@@ -7,11 +7,11 @@ class FluxoSimplesTestCase(TestCase):
         }
 
     def test_01_listar_eventos(self):
-        "Verifica se a rota de listagem de eventos está funcionando"
+        "Verifica se a rota de listagem de eventos"
         response = self.client.get('/eventos/')
         self.assertEqual(response.status_code, 200)
 
     def test_02_comprar_ingresso(self):
-        "Verifica se a rota de compra de ingresso está funcionando"
+        "Verifica se a rota de compra de ingresso"
         response = self.client.post('/ingressos/', self.dados_ingresso)
         self.assertIn(response.status_code, [200, 201])
